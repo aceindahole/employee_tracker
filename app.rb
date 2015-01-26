451cb701b175cd646c1d73e1884d107a39fa649d
Clem Freeman and Tyler Brown <elementalplug@gmail.com>
2015-01-26 13:42:57 -0800

    Create basic interface for adding/removing divisions and employees

--- app.rb
+++ app.rb
@@ -3,16 +3,30 @@ require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("sinatra/activerecord")
 require("./lib/employee")
+require("./lib/division")
 require("pg")
 
 
 get('/') do
   @employees = Employee.all()
+  @divisions = Division.all()
   erb(:index)
 end
 
-post('/employee') do
+# get('/employee/:id') do
+#   @employee = Employee.find(params.fetch("client_id").to_i())
+#   @divisions = Division.all()
+#   erb(:employee)
+# end
+
+post('/add_employee') do
   name = params.fetch('name')
   @employee = Employee.create({:name => name})
-  erb(:index)
+  redirect '/'
+end
+
+post('/add_division') do
+  name = params.fetch('name')
+  @division = Division.create({:name => name})
+  redirect '/'
 end
