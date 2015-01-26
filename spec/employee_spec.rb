85855b8ed32b0d114cf72ad27c067b67d24868a1
Clem Freeman and Tyler Brown <clemfree@gmail.com>
2015-01-26 10:42:12 -0800

    Implement Employee class

---
+++ spec/employee_spec.rb
@@ -0,0 +1,14 @@
+require("spec_helper")
+
+describe(Employee) do
+
+  describe(".create") do
+    it("adds a new instance of the Employee class and immediately saves it to the database") do
+      employee1 = Employee.create({:name => "Clem"})
+      employee2 = Employee.create({:name => "Ty-Ty"})
+      best_employees = [employee1, employee2]
+      expect(Employee.all()).to(eq(best_employees))
+    end
+  end
+
+end
