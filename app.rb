a4ab66694d4cb42aeef53e4380dc7f6a0d1ac788
Clem Freeman and Tyler Brown <clemfree@gmail.com>
2015-01-26 15:54:48 -0800

    Add some functionality to sinatra files, not fully linked

--- app.rb
+++ app.rb
@@ -13,11 +13,22 @@ get('/') do
   erb(:index)
 end
 
-# get('/employee/:id') do
-#   @employee = Employee.find(params.fetch("client_id").to_i())
-#   @divisions = Division.all()
-#   erb(:employee)
-# end
+get('/employee/:id') do
+  @employee = Employee.find(params.fetch("id").to_i())
+  @divisions = Division.all()
+  if @employee.division_id != nil
+    @division = Division.find(@employee.division_id)
+  else
+    @division = nil
+  end
+  erb(:employee)
+end
+
+get('/division/:id') do
+  @division = Division.find(params.fetch("id").to_i())
+  @employees = Employee.all()
+  erb(:division)
+end
 
 post('/add_employee') do
   name = params.fetch('name')
@@ -29,4 +40,11 @@ post('/add_division') do
   name = params.fetch('name')
   @division = Division.create({:name => name})
   redirect '/'
+end
+
+patch '/employee_division' do
+  division_id = params.fetch("division_id").to_i
+  employee_id = params.fetch("employee_id").to_i
+  Division.find(division_id).add_employee(Employee.find(employee_id))
+  redirect back
 end
